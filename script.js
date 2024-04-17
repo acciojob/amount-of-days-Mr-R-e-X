f1db590771b42c60f4cd6b6193599af8fa7dff9e
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-04-17 15:03:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+	return isLeapYear(year) ? 366 : 365
+}
+function isLeapYear(year) {
+	return year%400 === 0 || (year % 100 !== 0 && year % 4 === 0);
+}
+
+let year = promt("Enter a year")
+if(Number(year)!== NaN){
+	daysOfAYear(year);
+}
